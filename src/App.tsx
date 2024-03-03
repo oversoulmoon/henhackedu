705bb301c572558c2bb30001639af964156028f1
Trung Nguyen <trungtng@udel.edu>
2024-03-03 10:12:36 -0500

    finished ui and api integration

--- src/App.tsx
+++ src/App.tsx
@@ -1,106 +1,9 @@
 import React, { useState } from 'react';
-import logo from './logo.svg';
 import './App.css';
-import OpenAI from "openai";
 import MenuBar from './Menu';
 import Card from './Card';
 import axios from 'axios';
 import ChatRoom from './Chatroom';
-// const openai = new OpenAI({apiKey: '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',dangerouslyAllowBrowser: true});
-
-// const OpenAIQuestionAnswering: React.FC = () => {
-//   const [question, setQuestion] = useState<string>('');
-//   const [answer, setAnswer] = useState<string>('');
-
-//   const handleQuestionChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-//     setQuestion(e.target.value);
-//   };
-
-//   const handleSubmit = async (e: React.FormEvent) => {
-//     e.preventDefault();
-//     try {
-//       const response = await openai.chat.completions.create({
-//         model: "gpt-3.5-turbo",
-//         messages:[{role: 'user', content: question}],
-//         temperature: 0.7,
-//         n: 1
-//       });
-
-//       setAnswer(JSON.stringify(response.choices[0].message.content));
-//     } catch (error) {
-//       console.error('Error fetching data:', error);
-//     }
-//   };
-
-
-//   const menuItems = [
-//     { title: 'Home', link: '#home' },
-//     { title: 'About', link: '#about' },
-//     { title: 'Services', link: '#services' },
-//     { title: 'Contact', link: '#contact' }
-//   ];
-
-//   return (
-//     <div>
-//       <h1>Ask a Question</h1>
-//       <form onSubmit={handleSubmit}>
-//         <input type="text" value={question} onChange={handleQuestionChange} />
-//         <button type="submit">Submit</button>
-//       </form>
-//       {answer && (
-//         <div>
-//           <h2>Answer:</h2>
-//           <p>{answer}</p>
-//         </div>
-//       )}
-//     </div>
-//   );
-// };
-
-// function App() {
-
-//   const [videoUrl, setVideoUrl] = useState<string>('');
-//   const menuItems = [
-//     { title: 'Home', link: '#home' },
-//     { title: 'About', link: '#about' },
-//     { title: 'Services', link: '#services' },
-//     { title: 'Contact', link: '#contact' }
-//   ];
-
-//   const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-//     const file = event.target.files?.[0];
-//     if (file) {
-//       const videoUrl = URL.createObjectURL(file);
-//       setVideoUrl(videoUrl);
-//   };
-// }
-//   return (
-//     <div className="App">
-//       <MenuBar items={menuItems} />
-//       <div style={{ display: 'flex', justifyContent: 'space-around' }}>
-//       <Card>
-//         <h2>Upload Video</h2>
-//         <input type="file" accept="video/*" onChange={handleFileChange} />
-//         {videoUrl && (<video style={{width: '100%'}} controls>
-//           <source src={videoUrl} type="video/mp4" />
-//           Your browser does not support the video tag.
-//         </video>)}
-//       </Card>
-
-//       <Card>
-//         <h2>Section 2</h2>
-//         <p>This is the content of section 2.</p>
-//       </Card>
-//       <Card>
-//         <h2>Section 3</h2>
-//         <p>This is the content of section 3.</p>
-//       </Card>
-//       </div>
-//     </div>
-//   );
-// }
-
-// export default App;
 
 function App() {
 
@@ -109,20 +12,9 @@ function App() {
   const [VideoName, setVideoName] = useState<string>('Empty');
 
   const menuItems = [
-    { title: 'Home', link: '#home' },
-    { title: 'About', link: '#about' },
-    { title: 'Services', link: '#services' },
-    { title: 'Contact', link: '#contact' }
+    { title: 'VidExplainAI', link: '#' }
   ];
-    const [transcript, setTranscript] = useState<string>("");
-
-  const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const file = event.target.files?.[0];
-    if (file) {
-      const videoUrl = URL.createObjectURL(file);
-      setVideoUrl(videoUrl);
-  };
-}
+  const [transcript, setTranscript] = useState<string>("");
 
   const handleFileUpload = async (event: React.ChangeEvent<HTMLInputElement>) => {
     const file = event.target.files ? event.target.files[0] : null;
@@ -157,8 +49,12 @@ function App() {
           <Card>
           <input type="file" accept="video/*" id="actual-btn" onChange={handleFileUpload} hidden/>
           <label id="upload-btn" htmlFor="actual-btn">UPLOAD VIDEO</label>
-          <ChatRoom></ChatRoom>
           </Card>
+          <br/>
+          <Card>
+          <h3>Ask me anything</h3>
+              <ChatRoom>{transcript}</ChatRoom>
+            </Card>
         </div>
         <div className="three">        
           <Card>
@@ -182,42 +78,3 @@ function App() {
 }
 
 export default App;
-
-//
-
-// import React, { useState } from 'react';
-// import axios from 'axios';
-
-// const VideoUpload: React.FC = () => {
-//   const [transcript, setTranscript] = useState<string | null>(null);
-
-  // const handleFileUpload = async (event: React.ChangeEvent<HTMLInputElement>) => {
-  //   const file = event.target.files ? event.target.files[0] : null;
-  //   if (file) {
-      // const formData = new FormData();
-      // formData.append('file', file);
-
-      // try {
-      //   const response = await axios.post('http://localhost:5000/transcript', formData, {
-      //     headers: {
-      //       'Content-Type': 'multipart/form-data',
-      //     },
-      //   });
-      //   console.log(response.data.transcript)
-      //   setTranscript(response.data.transcript.text);
-      // } catch (error) {
-      //   console.error('Error uploading file:', error);
-      // }
-  //   }
-  // };
-
-
-//   return (
-//     <div>
-//       <input type="file" onChange={handleFileUpload} />
-//       {transcript && <div><strong>Transcript:</strong> {transcript}</div>}
-//     </div>
-//   );
-// };
-
-// export default VideoUpload
--- src/Chatroom.tsx
+++ src/Chatroom.tsx
@@ -1,50 +1,73 @@
-import React, { useState } from 'react';
-import './Chatroom.css'; // Import CSS file
+import React, { useState, FormEvent, useRef, useEffect } from 'react';
+import './Chatroom.css'
+import OpenAI from "openai";
+
+const openai = new OpenAI({apiKey: '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',dangerouslyAllowBrowser: true});
 
 interface Message {
   id: number;
   text: string;
-  sender: string;
+  sender: 'user' | 'bot'; // Assuming two types of senders
 }
-
-const ChatRoom: React.FC = () => {
+interface data {
+    children: string;
+}
+const SmsInterface: React.FC<data> = (props) => {
   const [messages, setMessages] = useState<Message[]>([]);
-  const [inputMessage, setInputMessage] = useState<string>('');
+  const divRef = useRef<null | HTMLDivElement>(null);
 
-  const sendMessage = () => {
-    if (inputMessage.trim() !== '') {
+  useEffect(() => {
+    //3️⃣ bring the last item into view        
+    divRef.current?.lastElementChild?.scrollIntoView()
+}, [messages]);
+
+  const handleMessageSend = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const messageInput = e.currentTarget.elements.namedItem('message') as HTMLInputElement;
+    const messageText = messageInput.value.trim();
+    if (messageText !== '') {
       const newMessage: Message = {
-        id: messages.length + 1,
-        text: inputMessage,
-        sender: 'User',
+        id: new Date().getTime(),
+        text: messageText,
+        sender: 'user',
       };
       setMessages([...messages, newMessage]);
-      setInputMessage('');
+      messageInput.value = '';
+    try {
+      const response = await openai.chat.completions.create({
+        model: "gpt-3.5-turbo",
+        messages:[{role: 'user', content: "Use this as context but do not say it in the response: " + props.children + messageText}],
+        temperature: 0.7,
+        n: 1
+      });
+      const newMessage2: Message = {
+        id: new Date().getTime(),
+        text: JSON.stringify(response.choices[0].message.content),
+        sender: 'bot',
+      };
+      setMessages([...messages, newMessage, newMessage2]);
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
     }
   };
 
   return (
-    <div className="chat-room">
-      <div className="chat-container">
-        <div className="message-container">
-          {messages.map(message => (
-            <div key={message.id} className={`message ${message.sender === 'User' ? 'sent' : 'received'}`}>
-              {message.text}
-            </div>
-          ))}
-        </div>
-        <div className="input-container">
-          <input
-            type="text"
-            value={inputMessage}
-            onChange={(e) => setInputMessage(e.target.value)}
-            placeholder="Type your message..."
-          />
-          <button onClick={sendMessage}>Send</button>
-        </div>
+    <div className="sms-interface">
+      <div className="messages" ref={divRef} >
+        {messages.map(message => (
+          <div key={message.id}  className={`message ${message.sender}`}>
+            {message.text}
+          </div>
+        ))}
+        <form onSubmit={handleMessageSend} className="message-form">
+        <input type="text" name="message" placeholder="Type a message..." className="message-input" />
+        <button type="submit" className="send-button">Send</button>
+      </form>
       </div>
+
     </div>
   );
 };
 
-export default ChatRoom;
+export default SmsInterface;
